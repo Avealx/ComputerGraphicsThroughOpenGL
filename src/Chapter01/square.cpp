f6cfb850e342a4a0f9fe3408f2fc18521f68dd04
Felix Herter <herter@zib.de>
2020-08-07 22:24:31 +0200

    [Add] restore pre Exercise 2.4

--- src/Chapter01/square.cpp
+++ src/Chapter01/square.cpp
@@ -38,7 +38,7 @@ void resize(int w, int h) {
 
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
-  glOrtho(-100.0, 100.0, 0.0, 100.0, -1.0, 1.0);
+  glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
 
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
@@ -64,7 +64,7 @@ int main(int argc, char **argv) {
 
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 
-  glutInitWindowSize(500, 250);
+  glutInitWindowSize(500, 500);
   glutInitWindowPosition(100, 100);
 
   glutCreateWindow("square.cpp");
